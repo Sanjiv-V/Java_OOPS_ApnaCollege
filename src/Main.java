0d3a54fe0ddfab1d7306b42cee1756b1463bbb62
Vinodkumar Sanjiv <vinodkumar.s@elfonze.com>
2025-09-29 17:43:26 +0530

    First commit, pre start

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main (String[] args){
+
+        // OOPS FROM APNA COLLEGE!
+        // https://www.youtube.com/watch?v=bSrm9RXwBaI
+
+
+
+    }
+
+
+}
